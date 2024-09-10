df92efe5982474c05b1d5b62477690829291e3e7
Simon Nguthu <simon.nguthu@student.moringaschool.com>
2024-09-10 23:43:44 +0300

    Organizing Bears

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "M", "Brown", "Calm", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 17, "M", "Brown", "Calm", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 15, "F", "Brown", "Calm", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 5, "M", "Brown", "Calm", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 10, "F", "Brown", "Calm", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch",65, "M", "Brown", "Calm", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 25, "F", "Brown", "Calm", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL , 12, "M", "Brown", "Calm", True);
